d7e1544cf4218b026bc1eee5c05b42500ae4ff27
Mohit Singh <mohitsinghraw8@gmail.com>
2026-02-10 16:13:20 +0530

    Created Store

--- frontend/src/redux/userSlice.js
+++ frontend/src/redux/userSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const userSlice =  createSlice({
-    nama:"user",
-    initialState:{
-        userData:nulll
+const userSlice = createSlice({
+    name: "user",
+    initialState: {
+        userData: null
     },
-    reducers:{
-        setUserData:(state,action)=>{
-            state.userData = action.payload
+    reducers: {
+        setUserData: (state, action) => {
+            state.userData = action.payload;
         }
-        }
-})
+    }
+});
 
-export const {setUserData} = userSlice.actions
-export default userSlice.reducer
+export const { setUserData } = userSlice.actions;
+export default userSlice.reducer;
